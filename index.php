84935afa45846d3254e4e5d0b49113ea90f3f449
Matt <matthias.etienne@gmail.com>
2012-03-19 01:38:59 +0100

    maj

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
       body { height: 100%; margin: 0; padding: 0 }
       #map_canvas { height: 100% }
     </style>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDnIsj5AWRoBSEtKoIWvoEUcb19tyZonMc&sensor=false"></script>
     <script type="text/javascript">
 	function initialize() {
@@ -20,6 +21,9 @@
 			mapTypeId: google.maps.MapTypeId.TERRAIN
 		};
 		var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		google.maps.event.addListener(map, 'click', function(event) {
+			console.log("[event][click]" + JSON.stringify(event));
+		});
 	}
     </script>
   </head>
