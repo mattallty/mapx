5bddbd62b44fe646baff51b230986402e1bba2ef
Matthias ETIENNE <matt@ad4s-p4.ad4s.local>
2012-03-27 16:26:00 +0200

    maj

---
+++ utils/update-database.php
@@ -0,0 +1,27 @@
+<?php
+$xml = simplexml_load_file("../inc/countryInfo.xml");
+$con = mysqli_connect("localhost", "root", "myphp2500bis");
+$db = mysqli_select_db($con, "mapx");
+
+$sql_tpl = "UPDATE _supa_geo_countries SET cou_continent = '%s', cou_capital = '%s', cou_square_km = %d, cou_population = %d WHERE cou_id = '%s'";
+
+foreach ($xml as $key => $val) {
+	
+	$sql = sprintf(	$sql, 
+		(string)$val->continent,
+		(string)$val->capital,
+		intval($val->areaInSqKm),
+		intval($val->population),
+		(string)$val->countryCode
+	);
+	
+	echo "Executing $sql ... ";
+	
+	if(mysqli_query($con, $sql)) {
+		echo "OK\n";
+	}else{
+		echo "ERR\n";
+	}
+	
+}
+?>
